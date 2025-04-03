dcec4f1664b07a72e098f0abc6c82898d0530169
semblant <robynhumphreys7_4@hotmail.com>
2025-04-02 22:38:09 -0600

    Add HTTP basic auth. There is weird bug I can't fix but the route is protected

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,6 @@
 class Admin::ProductsController < ApplicationController
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  http_basic_authenticate_with name:ENV['BASIC_AUTH_NAME'], password: ENV['BASIC_AUTH_PASSWORD']
 
   def index
     @products = Product.order(id: :desc).all
