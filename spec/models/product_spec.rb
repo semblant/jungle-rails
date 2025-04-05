ec7e80a705548a0e17b3cbf53f670324f3d79048
semblant <robynhumphreys7_4@hotmail.com>
2025-04-04 19:53:10 -0600

    Fix rspec Product tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,44 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  before(:each) do
-    @category = Category.create!(name: "Pothos")
-    @product = Product.new(
-      name: "Golden Pothos",
-      price_cents: 2958,
-      quantity: 7,
-      category_id: @category.id
-      )
-  end
-
-
   describe "validations" do
     it "should be valid with only necessary attributes" do
-      expect(@product).to be_valid
+      category = Category.create!(name: "Pothos")
+      product = Product.new(
+        name: "Golden Pothos",
+        price_cents: 2958,
+        quantity: 7,
+        category_id: category.id
+        )
+
+      expect(product).to be_valid
     end
 
     it "is invalid without a name" do
-      @product.name = nil
-      expect(@product).not_to be_valid
-      expect(@product.errors[:name]).to include("can't be blank")
+      category = Category.create!(name: "Pothos")
+      product = Product.new(
+        name: nil,
+        price_cents: 2958,
+        quantity: 7,
+        category_id: category.id
+      )
+
+      expect(product).not_to be_valid
+      expect(product.errors.full_messages).to include("Name can't be blank")
     end
 
     it "is invalid without a price" do
-      @product.price_cents = nil
-      expect(@product).not_to be_valid
-      expect(@product.errors[:price_cents]).to include("is not a number")
+      category = Category.create!(name: "Pothos")
+      product = Product.new(
+        name: "Golden Pothos",
+        price_cents: nil,
+        quantity: 7,
+        category_id: category.id
+        )
+      expect(product).not_to be_valid
+      expect(product.errors.full_messages).to include("Price is not a number")
     end
 
     it "is invalid without a quantity" do
-      @product.quantity = nil
-      expect(@product).not_to be_valid
-      expect(@product.errors[:quantity]).to include("can't be blank")
+      category = Category.create!(name: "Pothos")
+      product = Product.new(
+        name: "Golden Pothos",
+        price_cents: 2958,
+        quantity: nil,
+        category_id: category.id
+        )
+      expect(product).not_to be_valid
+      expect(product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it "is invalid without a category" do
-      @product.category = nil
-      expect(@product).not_to be_valid
-      expect(@product.errors[:category]).to include("must exist")
+      category = Category.create!(name: "Pothos")
+      product = Product.new(
+        name: "Golden Pothos",
+        price_cents: 2958,
+        quantity: 7,
+        category_id: nil
+        )
+      expect(product).not_to be_valid
+      expect(product.errors.full_messages).to include("Category must exist")
     end
   end
 end
